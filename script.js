e4423bc3b284c482f6672992bb5947a99086b711
Harith Onigemo <45686269+Harithmetic1@users.noreply.github.com>
2020-08-18 09:10:42 +0100

    khf project present

---
+++ script.js
@@ -0,0 +1,20 @@
+    AOS.init(); /*Refers to the Animate on Scroll Library for homepage*/
+
+
+    function openNav() {
+        document.getElementById("menu-icon").style.display = "none";
+    	document.getElementById("close-menu").style.display = "block";
+        document.getElementById("navUl").style.display = "grid";
+    }
+
+    function closeNav() {
+    	document.getElementById("menu-icon").style.display = "block";
+        document.getElementById("close-menu").style.display = "none";
+        document.getElementById("navUl").style.display = "none";
+    }
+    var icon = document.getElementById("hamburger");
+    
+    function clickOn(){
+        var nav = document.querySelector(".dropdown-content");
+        nav.classList.toggle("open");
+    };
